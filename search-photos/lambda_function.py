326765167e5bd63b6320e91d16ae2b5f49a04058
Nora Wang <46662317+daaabing@users.noreply.github.com>
2021-11-09 22:06:07 -0500

    Add files via upload

---
+++ search-photos/lambda_function.py
@@ -0,0 +1,59 @@
+
+import json
+import boto3
+import requests
+from requests_aws4auth import AWS4Auth
+from requests.auth import HTTPBasicAuth 
+
+region = 'us-west-2'
+service = 'es'
+credentials = boto3.Session().get_credentials()
+awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+HTTPBasicAuth
+# url = 'https://vpc-a2photos-vvsv5lqfof5dwmysbvnkemfyum.us-east-1.es.amazonaws.com/photos/_search'
+url = 'https://search-photos-wf47syehoy53qwgudl3fepdp2u.us-east-2.es.amazonaws.com/photos/_search'
+headers = { "Content-Type": "application/json" }
+lexruntime = boto3.client('lex-runtime')
+def lambda_handler(event, context):
+    print("event: ", event)
+    response = lexruntime.post_text(
+        botName='SearchBot',
+        botAlias='$LATEST',
+        userId='test',
+        inputText = event["queryStringParameters"]["q"],
+        sessionAttributes={},
+    )
+    slots = response['slots']["query"].split()
+    # slots = [event["queryStringParameters"]["q"]]
+    print(slots)
+    slots = [{ "match": { "labels":  s} } for s in slots]
+    params = {
+        "query": {
+            "function_score": {
+                "query": { "bool": { "should": slots } },
+                "random_score": {}
+            }
+        }
+    }
+    r = requests.get(url, auth=HTTPBasicAuth("chenbowen", "428810cBW#"), data=json.dumps(params), headers=headers)
+    # r = requests.get(url, auth=awsauth, data=json.dumps(params), headers=headers)
+    r = json.loads(r.text)
+    print(r)
+    r = r['hits']['hits']
+    res = [o["_source"]["objectKey"] for o in r]
+    print(res)
+    
+    return {
+        "statusCode": 200,
+        "headers": {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "*"
+        },
+        "body": json.dumps(res)
+    }
+            # "Access-Control-Allow-Origin": "http://chatbot6998cc.s3-website-us-east-1.amazonaws.com",
+
+
+    
